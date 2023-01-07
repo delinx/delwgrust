ceebabd9447bfae3f002426206770e96a4601e69
Mihails 'Delinx' Mozajevs <mihails@teamilluminate.dev>
2023-01-07 18:46:20 +0000

    git init

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+// include!("math/Matrix.rs");
+
+mod markdown
+{
+
+
+    pub fn parse(source: String) -> String
+    {
+        return source;
+    }
+}
+
+fn main()
+{
+    println!("Hello, world!");
+}
